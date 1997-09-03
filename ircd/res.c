299bf808d6174b973957dbf532d48300f7d4c942
kalt <kalt>
1997-09-03 18:20:18 +0000

    extended bad_hostname() to reject * and ?

--- ircd/res.c
+++ ircd/res.c
@@ -1446,7 +1446,8 @@ int len;
 	char	*s, c;
 
 	for (s = name; (c = *s) && len; s++, len--)
-		if (isspace(c) || (c == 0x7) || (c == ':'))
+		if (isspace(c) || (c == 0x7) || (c == ':') ||
+		    (c == '*') || (c == '?'))
 			return -1;
 	return 0;
 }
