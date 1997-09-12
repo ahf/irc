d067b393f32479c1d83f74722dfd37239d7d2f7c
kalt <kalt>
1997-09-12 02:09:24 +0000

    added 2 fields to Y lines for local/global limits per [user@]host

--- ircd/s_bsd.c
+++ ircd/s_bsd.c
@@ -597,8 +597,8 @@ Reg	char	*sockn;
  * Ordinary client access check. Look for conf lines which have the same
  * status as the flags passed.
  *  0 = Success
- * -1 = Access denied
- * -2 = Bad socket.
+ * -1 = Bad socket.
+ * -2 = Access denied
  */
 int	check_client(cptr)
 Reg	aClient	*cptr;
@@ -612,7 +612,7 @@ Reg	aClient	*cptr;
 		cptr->name, inetntoa((char *)&cptr->ip)));
 
 	if (check_init(cptr, sockname))
-		return -2;
+		return -1;
 
 	if (!IsUnixSocket(cptr))
 		hp = cptr->hostp;
--- ircd/s_err.c
+++ ircd/s_err.c
@@ -290,7 +290,7 @@ static	Numeric	numeric_replies[] = {
 /* 215 */	{ RPL_STATSILINE, "%c %s %s %s %d %d" },
 /* 216 */	{ RPL_STATSKLINE, "%c %s %s %s %d %d" },
 /* 217 */	{ RPL_STATSQLINE, "%c %s %s %s %d %d" },
-/* 218 */	{ RPL_STATSYLINE, "%c %d %d %d %d %ld" },
+/* 218 */	{ RPL_STATSYLINE, "%c %d %d %d %d %ld %d %d" },
 /* 219 */	{ RPL_ENDOFSTATS, "%c :End of /STATS report" },
 		{ 0, (char *)NULL },
 /* 221 */	{ RPL_UMODEIS, "%s" },
