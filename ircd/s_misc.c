b7a56bde107a9a6a6462eb51f40dda0d2c9fb561
kalt <kalt>
1997-12-23 15:36:09 +0000

    typo

--- ircd/s_misc.c
+++ ircd/s_misc.c
@@ -981,7 +981,7 @@ char	*name;
 	sendto_one(cptr,":%s %d %s :udp errors %u udp dropped %u",
 		   ME, RPL_STATSDEBUG, name, sp->is_udperr, sp->is_udpdrop);
 	sendto_one(cptr,
-	   ":%s %d %s : link checks %u passed %u 15s/%u 30s dropped %uSq/%uFl",
+	   ":%s %d %s :link checks %u passed %u 15s/%u 30s dropped %uSq/%uFl",
 		   ME, RPL_STATSDEBUG, name, sp->is_ckl, sp->is_cklq,
 		   sp->is_cklok, sp->is_cklQ, sp->is_cklno);
 	if (sp->is_wwcnt)
