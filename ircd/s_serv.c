bfdf20e3721f9ac41365f963930606770f40cb01
kalt <kalt>
1998-03-31 21:52:27 +0000

    removed obsolete USE_NJOIN

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -885,9 +885,7 @@ Reg	aClient	*cptr;
 				   acptr->user->username,
 				   acptr->user->host, stok,
 				   (*buf) ? buf : "+", acptr->info);
-#ifdef	USE_NJOIN
 			if ((cptr->serv->version & SV_NJOIN) == 0)
-#endif
 				send_user_joins(cptr, acptr);
 		    }
 		else if (IsService(acptr) &&
@@ -914,10 +912,8 @@ Reg	aClient	*cptr;
 		for (chptr = channel; chptr; chptr = chptr->nextch)
 			if (chptr->users)
 			    {
-#ifdef	USE_NJOIN
 				if (cptr->serv->version & SV_NJOIN)
 					send_channel_members(cptr, chptr);
-#endif
 				send_channel_modes(cptr, chptr);
 			    }
 	    }
