261bf7ec935ca4a9b4287347bcd6a132f126859a
kalt <kalt>
1998-04-05 21:58:10 +0000

    only send wallops to registered users

--- common/send.c
+++ common/send.c
@@ -1219,7 +1219,7 @@ void	sendto_ops_butone(aClient *one, aClient *from, char *pattern, ...)
 	bzero((char *)&sentalong[0], sizeof(int) * MAXCONNECTIONS);
 	for (cptr = client; cptr; cptr = cptr->next)
 	    {
-		if (IsService(cptr))
+		if (IsService(cptr) || !IsRegistered(cptr))
 			continue;
 		if ((IsPerson(cptr) && !SendWallops(cptr)) || IsMe(cptr))
 			continue;
