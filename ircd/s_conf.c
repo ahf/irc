83f3b9c88d60f40b044e88d165b87c3882bc8a20
kalt <kalt>
1998-04-15 18:51:55 +0000

    portability

--- ircd/s_conf.c
+++ ircd/s_conf.c
@@ -98,7 +98,7 @@ aClient *cptr;
                sendto_flag(SCH_LOCAL, "Ignoring bad mask: %s", mask);
                 return -1;
         }
-        lmask = htonl(0xfffffffful << (32 - m)); /* /24 -> 0xffffff00ul */
+        lmask = htonl((u_long)0xffffff00L << (32 - m)); /* /24->0xffffff00ul */
         baseip = htonl(i1 * 0x1000000 + i2 * 0x10000 + i3 * 0x100 + i4);
         return ((cptr->ip.s_addr & lmask) == baseip) ? 0 : 1;
 }
