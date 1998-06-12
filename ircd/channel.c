8d953315f469e98df9d0270a9c01033a7cd29944
kalt <kalt>
1998-06-12 22:59:57 +0000

    fixed join ,,, (too many channels) desynch bug

--- ircd/channel.c
+++ ircd/channel.c
@@ -1956,7 +1956,8 @@ char	*parv[];
 			   if already joined MAXCHANNELSPERUSER times. */
 			sendto_one(sptr, err_str(ERR_TOOMANYCHANNELS,
 				   parv[0]), name);
-			return 2;
+			/* can't return, need to send the info everywhere */
+			continue;
 		}
 
 		chptr = get_channel(sptr, name, CREATE);
