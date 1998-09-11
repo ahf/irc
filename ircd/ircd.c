00ac0514bce85e3cd142de921c2334fb9b63ab9a
kalt <kalt>
1998-09-11 18:27:20 +0000

    me.info from ircd.conf was overridden in setup_me()

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -513,7 +513,8 @@ aClient	*mp;
 	(void)get_my_name(mp, mp->sockhost, sizeof(mp->sockhost)-1);
 	if (mp->name[0] == '\0')
 		strncpyzt(mp->name, mp->sockhost, sizeof(mp->name));
-	me.info = strdup("IRCers United");
+	if (me.info == DefInfo)
+		me.info = strdup("IRCers United");
 	mp->lasttime = mp->since = mp->firsttime = time(NULL);
 	mp->hopcount = 0;
 	mp->authfd = -1;
