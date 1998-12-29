3da4e9bcb988e82a0371f722ae45b5cc70662164
kalt <kalt>
1998-12-29 15:09:23 +0000

    removed unused iauth code

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -172,21 +172,6 @@ void	server_reboot()
 #ifdef USE_SYSLOG
 	(void)closelog();
 #endif
-#if defined(USE_IAUTH)
-#if 0
-	if (adfd >= 0)
-	    {
-		/* if iauth is running, it'll become a zombie unless we wait
-		 * for it. (Of course, if the alarm rings, we haven't waited
-		 * long enough). -kalt
-		 */
-		close(adfd);
-		alarm(1);
-		wait(NULL);
-		alarm(0);
-	    }
-#endif
-#endif
 	for (i = 3; i < MAXCONNECTIONS; i++)
 		(void)close(i);
 	if (!(bootopt & (BOOT_TTY|BOOT_DEBUG)))
