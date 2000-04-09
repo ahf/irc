c71851fe2df80eb6a70127558453012e37212472
q <q>
2000-04-09 15:06:32 +0000

    removed some debug message

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -602,12 +602,9 @@ char	*nick, *username;
 		user->tok[1] = '\0';*/
 		sp = user->servp;
 	    }
-	else if (user->username != username)
-	    {
-		sendto_flag(SCH_DEBUG, 
-			"remote user with username != user->username");
+	else
 		strncpyzt(user->username, username, USERLEN+1);
-	    }
+
 	SetClient(sptr);
 	if (!MyConnect(sptr))
 /* && IsServer(cptr)) -- obsolete, old 2.8 protocol;
