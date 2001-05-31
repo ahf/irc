13f2ebeda11fd20bc92a135686bc83df66b55139
chopin <chopin>
2001-05-31 14:00:02 +0000

    logic flaw (|| instead of &&) in checking for ':ffff:' v4 mapped to v6 address made inetntop return null. remark: inetntop should NEVER return null!

--- common/support.c
+++ common/support.c
@@ -192,7 +192,7 @@ size_t the_size;
 	{
 		char	*p;
 
-		if (!(p = strstr(local_dummy, ":ffff:")) ||
+		if (!(p = strstr(local_dummy, ":ffff:")) &&
 			!(p = strstr(local_dummy, ":FFFF:")))
 		{
 			return NULL;	/* crash and burn */
