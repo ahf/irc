3978ff2543e2f6c18c8d004932afd8314a249579
jv <jv>
2002-06-02 00:44:52 +0000

    read_ircdtune(): Initialise size of _HOSTNAMEHASHSIZE to _HASHSIZE.

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -1319,6 +1319,8 @@ char *filename;
 		ww_size = t_data[0];
 		lk_size = t_data[1];
 		_HASHSIZE = t_data[2];
+		_HOSTNAMEHASHSIZE = t_data[2]; /* hostname has always same size
+						  as the client hash */
 		_CHANNELHASHSIZE = t_data[3];
 		_SERVERSIZE = t_data[4];
 		poolsize = t_data[5];
