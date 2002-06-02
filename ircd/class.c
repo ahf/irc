c6e2826d54c13741f3e0c808ed085aa972f17021
q <q>
2002-06-02 00:21:52 +0000

    Make maxSendq an int instead of long for now.

--- ircd/class.c
+++ ircd/class.c
@@ -135,8 +135,7 @@ aClass	*clptr;
  */
 void	add_class(class, ping, confreq, maxli, sendq, hlocal, uhlocal,
 		  hglobal, uhglobal)
-int	class, ping, confreq, maxli, hlocal, uhlocal, hglobal, uhglobal;
-long	sendq;
+int	class, ping, confreq, maxli, sendq, hlocal, uhlocal, hglobal, uhglobal;
 {
 	aClass *t, *p;
 
@@ -234,7 +233,7 @@ char	*to;
 			   MaxHGlobal(cltmp), MaxUHGlobal(cltmp));
 }
 
-long	get_sendq(cptr)
+int	get_sendq(cptr)
 aClient	*cptr;
 {
 	Reg	int	sendq = QUEUELEN, retc = BAD_CLIENT_CLASS;
