5a2e1dd8e5f8f75b44167ca0f32b86d512b6428f
chopin <chopin>
2003-10-10 22:32:05 +0000

    typo (%Full vs. %%Full) fix

--- ircd/hash.c
+++ ircd/hash.c
@@ -934,7 +934,7 @@ char	*parv[];
 		   parv[0], totlink, used_now, size);
 	if (!used_now)
 		used_now = 1;
-	sendto_one(sptr,"NOTICE %s :Hash Ratio (av. depth): %f %Full: %f",
+	sendto_one(sptr,"NOTICE %s :Hash Ratio (av. depth): %f %%Full: %f",
 		  parv[0], (float)((1.0 * totlink) / (1.0 * used_now)),
 		  (float)((1.0 * used_now) / (1.0 * size)));
 	sendto_one(sptr,"NOTICE %s :Deepest Link: %d Links: %d",
