8e58952299a8f50c56d12b258da18e4408ec77cc
chopin <chopin>
2003-10-12 22:20:06 +0000

    reverting code that made H: problems (Non-Hub link introducing...)

--- ircd/s_conf.c
+++ ircd/s_conf.c
@@ -542,8 +542,6 @@ int	statmask;
 
 	for (tmp = conf; tmp; tmp = tmp->next)
 	    {
-		if (IsIllegal(tmp))
-			continue;
 		if (!(tmp->status & statmask) || !tmp->name || !tmp->host ||
 		    mycmp(tmp->name, name))
 			continue;
@@ -617,8 +615,6 @@ int	statmask;
  
 	for (tmp = conf; tmp; tmp = tmp->next)
 	    {
-		if (IsIllegal(tmp))
-			continue;
 		/*
 		** Accept if the *real* hostname (usually sockecthost)
 		** matches *either* host or name field of the configuration.
@@ -644,8 +640,6 @@ int	statmask;
 	for (; lp; lp = lp->next)
 	    {
 		tmp = lp->value.aconf;
-		if (IsIllegal(tmp))
-			continue;
 		if ((tmp->status & statmask) &&
 		    (((tmp->status & (CONF_SERVER_MASK|CONF_HUB)) &&
 	 	     tmp->name && !mycmp(tmp->name, name)) ||
@@ -672,8 +666,6 @@ Reg	int	statmask;
 	for (; lp; lp = lp->next)
 	    {
 		tmp = lp->value.aconf;
-		if (IsIllegal(tmp))
-			continue;
 		if (tmp->status & statmask &&
 		    (!(tmp->status & CONF_SERVER_MASK || tmp->host) ||
 	 	     (tmp->host && !match(tmp->host, host))))
@@ -699,8 +691,6 @@ int	statmask;
 	for (; lp; lp = lp->next)
 	    {
 		tmp = lp->value.aconf;
-		if (IsIllegal(tmp))
-			continue;
 		if (!(tmp->status & statmask))
 			continue;
 		s = index(tmp->host, '@');
