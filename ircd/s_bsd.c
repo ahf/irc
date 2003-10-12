d1955bc5c81307f92ec8fdd3b2b555a75fdb3a99
chopin <chopin>
2003-10-12 13:27:05 +0000

    sig,h what made me use report_error(), I don't know.

--- ircd/s_bsd.c
+++ ircd/s_bsd.c
@@ -199,7 +199,7 @@ int	port;
 	(void)sscanf(ipmask, "%d.%d.%d.%d", &ad[0], &ad[1], &ad[2], &ad[3]);
 	if (ad[0]>>8 || ad[1]>>8 || ad[2]>>8 || ad[3]>>8)
 	{
-		report_error("invalid ipmask %s", ipmask);
+		sendto_flag(SCH_ERROR, "Invalid ipmask %s", ipmask);
 		return -1;
 	}
 	(void)sprintf(ipname, "%d.%d.%d.%d", ad[0], ad[1], ad[2], ad[3]);
