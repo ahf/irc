253c2197d7fa21de3a11b45a1c7b526c3722951a
jv <jv>
2003-10-18 13:04:19 +0000

    Cleaned up serveropts (removed SUN_GSO_BUG, NPATH, VALLOC and NOWRITEALARM)

--- ircd/s_debug.c
+++ ircd/s_debug.c
@@ -60,9 +60,6 @@ char	serveropts[] = {
 #ifdef	CLONE_CHECK
 'F',
 #endif
-#ifdef	SUN_GSO_BUG
-'g',
-#endif
 #ifdef	HUB
 'H',
 #endif
@@ -100,9 +97,6 @@ char	serveropts[] = {
 #ifdef	IDLE_FROM_MSG
 'M',
 #endif
-#ifdef	NPATH /* gone */
-'N',
-#endif
 #ifdef	BETTER_NDELAY
 'n',
 #endif
@@ -132,12 +126,6 @@ char	serveropts[] = {
 #endif
 #ifdef	ENABLE_USERS
 'U',
-#endif
-#ifdef	VALLOC
-'V',
-#endif
-#ifdef	NOWRITEALARM
-'w',
 #endif
 #ifdef	UNIXPORT
 'X',
