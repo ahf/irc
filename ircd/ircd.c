68f2173f8baa64ab10a37c97217bb8de14db24ac
chopin <chopin>
2004-01-02 15:52:27 +0000

    comment

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -1075,7 +1075,7 @@ static	void	io_loop(void)
 		nextconnect = try_connections(timeofday);
 	/*
 	** Every once in a while, hunt channel structures that
-	** can be freed.
+	** can be freed. Reop channels while at it, too.
 	*/
 	if (timeofday >= nextgarbage)
 		nextgarbage = collect_channel_garbage(timeofday);
