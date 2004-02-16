4a969ba499d91e4652c9d8f5fec32f5a09cbbec0
chopin <chopin>
2004-02-16 02:27:42 +0000

    boring to watch ;)

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -1896,7 +1896,7 @@ static	void	report_ping(aClient *sptr, char *to)
 				   buf, cp->lseq, cp->lrecvd,
 				   cp->ping / (cp->recvd ? cp->recvd : 1),
 				   tmp->pref);
-			sendto_flag(SCH_DEBUG, "%s: %d", buf, cp->seq);
+			/* sendto_flag(SCH_DEBUG, "%s: %d", buf, cp->seq); */
 		    }
 	return;
 }
