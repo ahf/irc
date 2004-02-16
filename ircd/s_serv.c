d05e4e70d07f28b430a6c4c5087a429673d9c346
chopin <chopin>
2004-02-16 11:05:05 +0000

    switching %s and %d (core) usercnt[2] is oper count -- not what we want (eh, jv)

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -3616,8 +3616,8 @@ static	void	dump_sid_map(aClient *sptr, aClient *root, char *pbuf)
 	}
 	else
 	{
-		sprintf(pbuf, "%s %d %s %s%s%s", root->name, root->serv->sid,
-			  root->serv->usercnt[0] + root->serv->usercnt[2],
+		sprintf(pbuf, "%s %s %d %s%s%s", root->name, root->serv->sid,
+			  root->serv->usercnt[0] + root->serv->usercnt[1],
 			  root->serv->verstr[0] ? " " : "",
 			  root->serv->verstr,
 			  IsBursting(root) ? " BURST" : "");
