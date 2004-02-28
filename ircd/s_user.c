c6098e632a63518d780530e93635bae0dac4ae0f
chopin <chopin>
2004-02-28 01:14:39 +0000

    we did not check do_nick_name return value (which is important, if 0)

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -1253,8 +1253,7 @@ int	m_unick(aClient *cptr, aClient *sptr, int parc, char *parv[])
 	 * creation) then reject it. If from a server and we reject it,
 	 * and KILL it. -avalon 4/4/92
 	 */
-	do_nick_name(nick, 1);
-	if (strcmp(nick, parv[1]))
+	if (do_nick_name(nick, 1) == 0 || strcmp(nick, parv[1]))
 	{
 		sendto_one(sptr, replies[ERR_ERRONEOUSNICKNAME], ME, BadTo(parv[0]),
 			parv[1]);
