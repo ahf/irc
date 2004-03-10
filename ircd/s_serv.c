a0e2ad3140ed2d9902a0cdadd26ff7fc381ceff6
chopin <chopin>
2004-03-10 19:25:49 +0000

    add #ifdef DELAY_CLOSE :)

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -3042,10 +3042,12 @@ int	m_close(aClient *cptr, aClient *sptr, int parc, char *parv[])
 	sendto_one(sptr, replies[RPL_CLOSEEND], ME, BadTo(parv[0]), closed);
 	sendto_flag(SCH_NOTICE, "%s closed %d unknown connections", sptr->name,
 		    closed);
+#ifdef DELAY_CLOSE
 	closed = istat.is_delayclosewait;
 	delay_close(-2);
 	sendto_flag(SCH_NOTICE, "%s closed %d delayed connections", sptr->name,
 		    closed);
+#endif
 	return 1;
 }
 
