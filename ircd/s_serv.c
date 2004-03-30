6bea4c1d1488796387234b95fc418c339a3b298d
chopin <chopin>
2004-03-30 13:25:03 +0000

    allow stats C from remote

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -3775,20 +3775,17 @@ static void report_listeners(aClient *sptr, char *to)
 	}
 }
 
+/* Reports class usage */
 static void report_class(aClient *sptr, char *to)
 {
-	/* Report Class usage */
-	if (IsPerson(sptr))
-	{
-		aClass  *tmp;
+	aClass  *tmp;
 
-	    	for (tmp = FirstClass(); tmp; tmp = NextClass(tmp))
-	    	{
-			if (Links(tmp) > 0)
-			{
-				sendto_one(sptr, replies[RPL_TRACECLASS],
-					ME, to, Class(tmp), Links(tmp));
-			}
-   		}
+	for (tmp = FirstClass(); tmp; tmp = NextClass(tmp))
+	{
+		if (Links(tmp) > 0)
+		{
+			sendto_one(sptr, replies[RPL_TRACECLASS],
+				ME, to, Class(tmp), Links(tmp));
+		}
 	}
 }
