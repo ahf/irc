2536d248b61b9dc0da4ad638f497c78966b1b6b7
chopin <chopin>
2004-06-11 23:36:25 +0000

    param count is checked in parse

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -1537,19 +1537,7 @@ static	int	m_message(aClient *cptr, aClient *sptr, int parc,
 	char	*nick, *server, *p, *cmd, *user, *host;
 	int	count = 0, penalty = 0, syntax = 0;
 
-	cmd = notice ? MSG_NOTICE : MSG_PRIVATE;
-
-	if (parc < 2 || *parv[1] == '\0')
-	    {
-		sendto_one(sptr, replies[ERR_NORECIPIENT], ME, BadTo(parv[0]), cmd);
-		return 1;
-	    }
-
-	if (parc < 3 || *parv[2] == '\0')
-	    {
-		sendto_one(sptr, replies[ERR_NOTEXTTOSEND], ME, BadTo(parv[0]));
-		return 1;
-	    }
+	cmd = notice ? "NOTICE" : "PRIVMSG";
 
 	if (MyConnect(sptr))
 		parv[1] = canonize(parv[1]);
