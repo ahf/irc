3d45a77c0c4358819581ead7713b46161f4db0c4
chopin <chopin>
2004-06-11 18:00:59 +0000

    remove c_*.h, no longer needed

--- common/match.c
+++ common/match.c
@@ -22,17 +22,9 @@ static  char rcsid[] = "@(#)$Id$";
 #endif
 
 #include "os.h"
-#ifndef CLIENT_COMPILE
-# include "s_defines.h"
-#else
-# include "c_defines.h"
-#endif
+#include "s_defines.h"
 #define MATCH_C
-#ifndef CLIENT_COMPILE
-# include "s_externs.h"
-#else
-# include "c_externs.h"
-#endif
+#include "s_externs.h"
 #undef MATCH_C
 
 unsigned char tolowertab[] =
--- common/support.c
+++ common/support.c
@@ -22,17 +22,9 @@ static  char rcsid[] = "@(#)$Id$";
 #endif
 
 #include "os.h"
-#ifndef CLIENT_COMPILE
-# include "s_defines.h"
-#else
-# include "c_defines.h"
-#endif
+#include "s_defines.h"
 #define SUPPORT_C
-#ifndef CLIENT_COMPILE
-# include "s_externs.h"
-#else
-# include "c_externs.h"
-#endif
+#include "s_externs.h"
 #undef SUPPORT_C
 
 unsigned char minus_one[]={ 255, 255, 255, 255, 255, 255, 255, 255, 255,
