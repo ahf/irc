236b73a6fc85733da43845cd22bae8245d6f0ce9
chopin <chopin>
2004-07-03 16:00:38 +0000

    parse new ACL flags

--- ircd/s_conf.c
+++ ircd/s_conf.c
@@ -259,6 +259,10 @@ char	*oline_flags_to_string(long flags)
 	if (flags & ACL_CLIENTS)
 		*s++ ='&';
 #endif
+	if (flags & ACL_NOPENALTY)
+		*s++ = 'P';
+	if (flags & ACL_CANFLOOD)
+		*s++ = 'p';
 	if (s == ofsbuf)
 		*s++ = '-';
 	*s++ = '\0';
@@ -293,6 +297,8 @@ long	oline_flags_parse(char *string)
 #ifdef CLIENTS_CHANNEL
 		case '&': tmp |= ACL_CLIENTS; break;
 #endif
+		case 'P': tmp |= ACL_NOPENALTY; break;
+		case 'p': tmp |= ACL_CANFLOOD; break;
 		}
 	}
 	return tmp;
