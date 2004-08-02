65daa8b074c48c64a1e672fcd6a4ba8fb5e6c002
chopin <chopin>
2004-08-02 17:42:08 +0000

    we change parv directly (disguised as cp, then as lp->value.cp), so we have to restore it, so another channel loop in m_mode could use it again.

--- ircd/channel.c
+++ ircd/channel.c
@@ -1614,6 +1614,10 @@ static	int	set_mode(aClient *cptr, aClient *sptr, aChannel *chptr,
 				if ((host = rindex(user ? user : cp, '@')))
 					*host++ = '\0';
 				lp->value.alist = make_bei(cp, user, host);
+				if (user)
+					user[-1] = '!';
+				if (host)
+					host[-1] = '@';
 				break;
 			case MODE_KEY :
 				c = 'k';
