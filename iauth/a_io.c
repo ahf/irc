36849e62be3aa554a9048b255df383b7481e8a8d
chopin <chopin>
2004-09-21 22:29:49 +0000

    bah! double-check not to commit quick test hacks

--- iauth/a_io.c
+++ iauth/a_io.c
@@ -170,14 +170,14 @@ static	void	next_io(int cl, AnInstance *last)
 	    /* sixth, we've got an instance to try */
 	{
 	    int r;
-	    static delayedsent = 0;	/* one fake is enough */
 
-	    if (!delayedsent && cldata[cl].instance->delayed)
+	    if (cldata[cl].instance->delayed &&
+			!(cldata[cl].state & A_DELAYEDSENT))
 		{
 		    /* fake to ircd that we're done */
 		    sendto_ircd("D %d %s %u ", cl, cldata[cl].itsip,
 				cldata[cl].itsport);
-		    delayedsent = 1;
+		    cldata[cl].state |= A_DELAYEDSENT;
 		}
 
 	    cldata[cl].timeout = time(NULL) + cldata[cl].instance->timeout;
