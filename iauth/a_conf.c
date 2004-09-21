0898eb14503c71270207b091ce979ddefe373eb5
chopin <chopin>
2004-09-21 13:59:51 +0000

    port mandatory for webproxy

--- iauth/a_conf.c
+++ iauth/a_conf.c
@@ -398,7 +398,8 @@ char	*conf_read(char *cfile)
 				(*ttmp)->nextt = NULL;
 			    }
 			if ((*last)->port == 0 &&
-				Mlist[i] == &Module_socks)
+				(Mlist[i] == &Module_webproxy ||
+				Mlist[i] == &Module_socks))
 			{
 				conf_err(lnnb, "port here is mandatory.", cfile);
 			}
