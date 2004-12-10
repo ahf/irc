c3b66ee8494cf945a6a3dbd3cba80510ac556703
chopin <chopin>
2004-12-10 14:39:04 +0000

    tame notices about maxsendq exceeded, allow for complete disabling via define

--- common/send.c
+++ common/send.c
@@ -149,10 +149,21 @@ int	send_message(aClient *to, char *msg, int len)
 		{
 			/* Allow bursting clients (servers or services) to
 			 * exceed their maxsendq. --B. */
-			sendto_flag(SCH_NOTICE,
-				"Max SendQ limit exceeded for %s: %d > %d",
-				to->name,
-				DBufLength(&to->sendQ), get_sendq(to));
+#  ifdef MAXSENDQ_NOTICES
+			static	time_t	lastnotice = 0;
+
+			/* It's total, not per destination, so it can
+			 * happen some notices escape us. Better this
+			 * than getting 50Hz notices. --B. */
+			if (timeofday - lastnotice >= 60)
+			{
+				sendto_flag(SCH_NOTICE, "Max SendQ limit "
+					"exceeded for %s: %d > %d",
+					to->name,
+					DBufLength(&to->sendQ), get_sendq(to));
+				lastnotice = timeofday;
+			}
+#  endif
 		}
 		else
 # endif
