da48606a328736c322d02c12d73de721c0744790
chopin <chopin>
2005-02-04 18:08:48 +0000

    LOCALNICKLEN 12

--- common/struct_def.h
+++ common/struct_def.h
@@ -41,13 +41,7 @@ typedef struct        LineItem aExtData;
 
 #define	HOSTLEN		63	/* Length of hostname.  Updated to         */
 				/* comply with RFC1123                     */
-
-#define	NICKLEN		15
-#define ONICKLEN	9	/* this is compatibility NICKLEN, allowed
-				** from local clients; we are prepared for
-				** longer nicks (longer NICKLEN), but do not
-				** yet allow them, as older (pre 2.11) servers
-				** would simply KILL them */
+#define	NICKLEN		15	/* Must be the same network-wide. */
 #define UIDLEN		9	/* must not be bigger than NICKLEN --Beeth */
 #define	USERLEN		10
 #define	REALLEN	 	50
@@ -965,6 +959,9 @@ typedef enum ServerChannels {
 #endif
 #if ! (UIDLEN > SIDLEN)
 #   error UIDLEN must be bigger than SIDLEN
+#endif
+#if (NICKLEN < LOCALNICKLEN)
+#   error LOCALNICKLEN must not be bigger than NICKLEN
 #endif
 
 /*
