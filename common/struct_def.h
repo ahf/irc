f959eef12fe463899b24e62509f86dd9579849a0
jv <jv>
2007-04-10 11:08:20 +0000

    Added is_rreop to struct stats.

--- common/struct_def.h
+++ common/struct_def.h
@@ -585,6 +585,7 @@ struct	stats {
 	u_int	is_save; /* number of saved clients */
 	u_int	is_fake; /* MODE 'fakes' */
 	u_int	is_reop; /* number of local reops */
+	u_int	is_rreop; /* number of remote reops */
 	u_int	is_asuc; /* successful auth requests */
 	u_int	is_abad; /* bad auth requests */
 	u_int	is_udpok;	/* packets recv'd on udp port */
